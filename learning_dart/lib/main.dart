e75d317d6333e898cf8ac861354c0dbac34ef7e5
Mike Odnis <airwolf635@gmail.com>
2023-10-04 05:46:45 -0400

    quick

--- learning_dart/lib/main.dart
+++ learning_dart/lib/main.dart
@@ -108,7 +108,7 @@ void main() {
 //   pairOfStrings.first;
 // }
 
-class MyApp extends StatelessWidget {
+  class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
